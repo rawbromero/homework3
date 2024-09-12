1960c9caeaf4df47bfb0018f0ea1289943723c83
bobbyrom33197@yahoo.com <bobbyrom33197@yahoo.com>
2024-09-11 19:29:12 -0500

    first commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref} from 'vue'
+const name = ref('')
+</script>
+
+<template>
+ 
+ <main class="app">
+
+   <section class="greetng">
+    <h2 class="tittle">
+      Welcome back,  <input type="text" placeholder="Enter Name" v-model="name">
+    </h2>
+
+   </section>
+   <section class="create-todo">
+
+   </section>
+
+   <section class="todo-list">
+
+   </section>
+
+ </main>
+</template>
+
